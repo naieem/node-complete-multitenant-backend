6dba83e4e22de69c9a8000ce150b64ef45e34fb0
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-05-13 12:18:55 +0600

    data updated

--- handler/data.handler.js
+++ handler/data.handler.js
@@ -818,64 +818,82 @@ function generatePdf(req) {
     return new Promise((resolve, reject) => {
         const width = 900;
         const height = 400;
+        // const configuration = {
+        //     type: 'line',
+        //     data: {
+        //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        //         datasets: [{
+        //                 label: 'Pending',
+        //                 data: [
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor()
+        //                 ],
+        //                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        //                 borderColor: 'rgba(255,99,132,1)',
+        //                 borderWidth: 1
+        //             },
+        //             {
+        //                 label: 'Done',
+        //                 data: [
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor()
+        //                 ],
+        //                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        //                 borderColor: 'rgba(54, 162, 235, 1)',
+        //                 borderWidth: 1
+        //             },
+        //             {
+        //                 label: 'In Progress',
+        //                 data: [
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor(),
+        //                     randomScalingFactor()
+        //                 ],
+        //                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        //                 borderColor: 'rgba(255, 206, 86, 1)',
+        //                 borderWidth: 1
+        //             }
+        //         ]
+        //     },
+        //     options: {
+        //         scales: {
+        //             xAxes: [{
+        //                 stacked: false,
+        //             }],
+        //             yAxes: [{
+        //                 stacked: true
+        //             }]
+        //         }
+        //     }
+        // };
         const configuration = {
             type: 'line',
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [{
-                        label: 'Pending',
-                        data: [
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor()
-                        ],
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Done',
-                        data: [
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor()
-                        ],
-                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                        borderColor: 'rgba(54, 162, 235, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'In Progress',
-                        data: [
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor(),
-                            randomScalingFactor()
-                        ],
-                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                        borderColor: 'rgba(255, 206, 86, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
+            data: req.body.pdfData,
             options: {
                 scales: {
                     xAxes: [{
                         stacked: false,
                     }],
                     yAxes: [{
-                        stacked: true
+                        stacked: true,
+                        ticks: {
+                            beginAtZero: true,
+                            callback: (value) => '$' + value
+                        }
                     }]
                 }
             }
