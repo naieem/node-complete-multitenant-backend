4c57100373045e7277962056a0a4ea1109db98cd
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-07-10 12:59:17 +0600

    cookie enabled validation middleware

--- services/middleware.service.js
+++ services/middleware.service.js
@@ -3,10 +3,18 @@ var tokenStorage = require('./tokenStorage.service');
 module.exports = {
     validatetoken: validatetoken
 }
-
+/**
+ * logic behind this.
+ * if authorization presents in the header then use that as token.
+ * if not then check for cookies.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 function validatetoken(req, res, next) {
-    if (req.headers && req.headers.authorization) {
-        if (!tokenStorage.checkExpiredToken(req.headers.authorization)) {
+    let token = req.headers && req.headers.authorization ? req.headers.authorization : (req.cookies && req.cookies[req.headers.origin] ? req.cookie[req.headers.origin] : null )
+    if (token) {
+        if (!tokenStorage.checkExpiredToken(token)) {
             authService
                 .validateToken(req.headers.authorization)
                 .then(function(result) {
