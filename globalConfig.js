eb5219fbc2c56b9cb32cd6617542be535e891862
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-07-18 19:59:22 +0600

    product manager table added

--- globalConfig.js
+++ globalConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     tokenExpiryTime: 50,
     defaultTotalRowCount: 10,
-    localdb: 'mongodb://localhost:27017/ITSM',
+    localdb: 'mongodb://localhost:27017/productmanager',
     storageLocation: "C:\storage",
     mailServiceUrl:"http://localhost:3000/api/sendMail"
 }
