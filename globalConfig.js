3789f34931bbac958cd7b2cdb056bf045a413f1c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-07-23 19:34:21 +0600

    db name updated

--- globalConfig.js
+++ globalConfig.js
@@ -1,7 +1,8 @@
 module.exports = {
     tokenExpiryTime: 50,
     defaultTotalRowCount: 10,
-    localdb: 'mongodb://supto:1qazZAQ!@ds351807.mlab.com:51807/carvault',
+    // localdb: 'mongodb://supto:1qazZAQ!@ds351807.mlab.com:51807/carvault',
+    localdb: 'mongodb://localhost:27017/ITSM',
     storageLocation: "./storage/",
     mailServiceUrl:"http://localhost:3000/api/sendMail"
 }
