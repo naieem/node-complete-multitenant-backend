863d5b0dc973515d2d66fa176581154cbd6d038a
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-07-24 18:13:48 +0600

    account verification route added

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,10 @@ var faker = require("faker");
 
 var globalConfig = require("../globalConfig");
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, globalConfig.storageLocation);
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         let filename = file.originalname.split(',');
         let fileId = faker.random.uuid();
         file['filename'] = fileId + filename[filename.length - 1];
@@ -16,12 +16,14 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+    storage: storage
+});
 var autoController = require('../controller/auth.controller');
 var dataController = require('../controller/data.controller');
 var middleware = require('../services/middleware.service');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.json({
         result: 'yea it works'
     });
@@ -47,11 +49,57 @@ router.post('/generatePdf', middleware.validatetoken, dataController.generatePdf
 router.post('/updateUserInfo', middleware.validatetoken, dataController.updatePassword);
 router.post('/sendMail', middleware.validatetoken, dataController.sendMail);
 router.post('/getanonymouseToken', dataController.getanonymousToken);
+/**
+ * account activation handler
+ */
+router.post('/accountVerification', (req, res,next) => {
+    let key = req.body.verificationKey;
+    var tablename = req.body.table;
+    var data = req.body.data;
+    AppCache.get(key, (error, result) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json({
+                status: false,
+                message: error
+            });
+        } else {
+            if(!result){
+                res.status(500).json({
+                    status: false,
+                    message: "User activation failed"
+                }); 
+            }
+            else if(result =="active"){
+                res.status(500).json({
+                    status: false,
+                    message: "User is already activated"
+                }); 
+            }else{
+                tables[origin][tablename].updateOne({
+                    _id: data._id
+                  },
+                  data,
+                  (err, response) => {
+                    if (err) return reject(err);
+            
+                    if (response.ok)
+                    res.status(200).json({
+                        status: true,
+                        itemId: data._id,
+                        message: "User Activation succesfull"
+                      });
+                  }
+                );
+            }
+        }
+    })
+});
 router.post('/validateToken', middleware.validatetoken, (req, res) => {
     res.status(200).send({
         result: true,
         message: 'Login successfull',
-        userInfo:req.userInfo
+        userInfo: req.userInfo
     });
 });
 module.exports = router;
