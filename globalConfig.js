a577a556ec5ec750b339844ce3c502ca5509de15
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-07-30 23:47:55 +0600

    port updated mail servic

--- globalConfig.js
+++ globalConfig.js
@@ -4,5 +4,5 @@ module.exports = {
     localdb: 'mongodb://supto:1qazZAQ!@ds351807.mlab.com:51807/carvault',
     // localdb: 'mongodb://localhost:27017/ITSM',
     storageLocation: "./storage/",
-    mailServiceUrl:"http://localhost:3000/api/sendMail"
+    mailServiceUrl:"http://localhost:4000/api/sendMail"
 }
