d7cc8ba14b46695e258172dfc037120a2c4813b6
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-08-01 22:58:05 +0600

    verifyEmailUrl added to cache and service

--- services/auth.service.js
+++ services/auth.service.js
@@ -53,10 +53,11 @@ function register(userInfo, personInfo, request) {
                                 // for storing temporary cache value
                                 AppCache.set(personInfo.user_id, "inactive", 60 * 5);
                                 // =============== sending new user registration mail ================= //
+                                let verifyEmailUrl=AppCache.get(request.headers.origin).verifyEmailUrl;
                                 let mailBody=`
                                 <h2>Hello ${personInfo.firstname} ${personInfo.lastname}</h2>
                                 <h3>Thank you for registering.</h3><p>To activate your account please click the link below</p>
-                                <p><a href="${request.headers.origin}/verifyAccount/${personInfo.user_id}">Activation link</a></p>
+                                <p><a href="${verifyEmailUrl}/${personInfo.user_id}">Activation link</a></p>
                                 <p><b>NB.</b>To remind you that the link will be deactivated within 5 mins</p>
                                 `;
                                 let mailPayload = new FormData();
@@ -85,7 +86,8 @@ function register(userInfo, personInfo, request) {
                                 resolve({
                                     status: true,
                                     personId: response.itemId,
-                                    userId: personInfo.user_id
+                                    userId: personInfo.user_id,
+                                    message:'User registration succesfull'
                                 });
                             }).catch((err) => {
                                 resolve({
